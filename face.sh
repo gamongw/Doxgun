ba3609b7139fd7f627831aafb36caa288bf12d59
Akudama <141340695+akudama8080@users.noreply.github.com>
2023-11-03 20:32:04 +0800

    Add files via upload

---
+++ face.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+
+echo "[+] LOADING...."
+echo " "
+echo "https://www.google.com/imghp?hl=en "
+echo " "
+echo "https://yandex.com/images/touch"
+echo " "
+echo "https://www.bing.com/visualsearch "
+echo " "
+echo "https://pimeyes.com/en"
